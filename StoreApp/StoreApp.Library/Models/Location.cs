5fdbd7ce1bdfd4d7b93e6609d8711673dafa1466
arman <arman97rm@gmail.com>
2020-06-28 03:36:55 -0400

    defined Location class

--- StoreApp/StoreApp.Library/Models/Location.cs
+++ StoreApp/StoreApp.Library/Models/Location.cs
@@ -6,6 +6,9 @@ namespace StoreApp.Library.Models
 {
     public class Location
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Dictionary<Product,int> StoreInventory { get; set; }
         public static Product GetProduct(string product)
         {
             throw new NotImplementedException();
