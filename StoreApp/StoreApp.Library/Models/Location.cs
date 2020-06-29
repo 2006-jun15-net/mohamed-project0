44c2e1ecab183ca0e4a7f977b8a73497958f6635
arman <arman97rm@gmail.com>
2020-06-29 15:46:25 -0400

    functionality

--- StoreApp/StoreApp.Library/Models/Location.cs
+++ StoreApp/StoreApp.Library/Models/Location.cs
@@ -9,14 +9,15 @@ namespace StoreApp.Library.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public static Dictionary<Product,int> StoreInventory { get; set; }
-        public static Product GetProduct(string product)
+        public static Product GetProduct(string product, int quantity)
         {
             try
             {
                 foreach (KeyValuePair<Product, int> item in StoreInventory)
                 {
-                    if (item.Key.Name == product)
+                    if (item.Key.Name == product && item.Value>= quantity)
                     {
+                        StoreInventory[item.Key] -= quantity;
                         return item.Key;
                     }
                 }
